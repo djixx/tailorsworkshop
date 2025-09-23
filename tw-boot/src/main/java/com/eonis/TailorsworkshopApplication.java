d8fd5cebda64ba1888ce37d627986feadf5ce9d6
djixx <99146360+djixx@users.noreply.github.com>
2025-09-23 18:07:58 +0200

    Implemented hexagonal architecture

--- tw-boot/src/main/java/com/eonis/TailorsworkshopApplication.java
+++ tw-boot/src/main/java/com/eonis/TailorsworkshopApplication.java
@@ -1,4 +1,4 @@
-package com.eonis.tailorsworkshop;
+package com.eonis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TailorsworkshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TailorsworkshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TailorsworkshopApplication.class, args);
+    }
 
 }
